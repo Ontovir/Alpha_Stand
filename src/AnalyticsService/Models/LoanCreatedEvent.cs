15ba82de264e393e1095ec0039f3086f4b5c241a
Aleksei Vindiurin <avi@itoolabs.com>
2025-11-04 13:22:43 +0300

    LoanCreatedEvent doubles the same in LoanService

---
+++ src/AnalyticsService/Models/LoanCreatedEvent.cs
@@ -0,0 +1,12 @@
+namespace LoanService.Models;
+
+/// <summary>
+/// Событие создания займа из Kafka
+/// </summary>
+public sealed record LoanCreatedEvent
+{
+    public required int LoanId { get; init; }
+    public required int UserId { get; init; }
+    public required decimal Amount { get; init; }
+    public required DateTime CreatedAt { get; init; }
+}
